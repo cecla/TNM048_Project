1ca5e98274172514d48bf761bdbab612c75b9b80
cecla <cicci.lagerwall@gmail.com>
2017-02-22 11:35:21 +0100

    added a population pie

---
+++ js/piePop.js
@@ -0,0 +1,86 @@
+function piePop(){
+	var self = this;
+	
+	var pieDiv = $("#piepop");
+
+	var colors = d3.scale.category20();
+
+	var width = 960,
+		height = 700,
+		radius = Math.min(width, height) / 4;
+
+	var toolTip = d3.select("body").append("div")   
+        .attr("class", "tooltip")               
+        .style("opacity", 0);
+
+    var arc = d3.svg.arc()
+		.outerRadius(radius - 10)
+		.innerRadius(0);
+
+	var labelArc = d3.svg.arc()
+		.outerRadius(radius - 40)
+		.innerRadius(radius - 40);
+
+	var pie = d3.layout.pie()
+		.sort(null)
+		.value(function(d) { console.log(d); return d.values; });
+
+	var svg = d3.select("body").append("svg")
+		.attr("width", width)
+		.attr("height", height)
+		.append("g")
+		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+	d3.csv("data/Swedish_Population_Statistics.csv", function(data) {
+		
+		
+		self.data = data;
+
+		console.log(data);
+
+		var temp = d3.nest()
+			.key(function(d){
+				return d.region;
+			})
+			.key(function(d){
+				return d["marital status"];
+			})
+			.rollup(function(v){ return d3.sum(v, function(d){return d["2010"]; })})
+			.entries(data);
+
+		console.log(temp);
+		draw(temp[0].values);
+		
+    });
+
+    function draw(population){
+    	var g = svg.selectAll(".arc")
+		.data(pie(population))
+		.enter().append("g")
+		 .attr("class", "arc");
+
+		g.append("path")
+		.attr("d", arc)
+		.style("fill", function(d) {  console.log(d.data.key); return colors(d.data.key); });
+
+		g.append("text")
+		  .attr("transform", function(d) { console.log(d);return "translate(" + labelArc.centroid(d) + ")"; })
+		  .attr("dy", ".35em")
+		  .text(function(d){ console.log(d);return d.data.key; });
+    }
+
+    /*function type(d) {
+		if(d["Year=2010"] != "..")
+		{
+			d["Year=2010"] = +d["Year=2010"];
+			
+		}
+		else
+		{
+			d["Year=2010"] = 0;
+			
+		}
+		
+		return d;
+	}*/
+}
