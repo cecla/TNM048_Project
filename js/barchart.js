91bb4365006cf08cb8d3aababe621662d64c1649
cecla <cicci.lagerwall@gmail.com>
2017-02-26 18:16:09 +0100

    fixed barchart

--- js/barchart.js
+++ js/barchart.js
@@ -1,16 +1,39 @@
-function barchart(){
+function barchart(data){
 
 	var self = this;
+	self.data = data;
 
-	var barDiv = $("#bar");
+	//console.log(self.data);
 
 	var toolTip = d3.select("body").append("div")   
         .attr("class", "tooltip")               
         .style("opacity", 0);
 
-    var margin = {top: 100, right: 40, bottom: 100, left: 40},
+    var barDiv = $("#bar");
+
+    var margin = {top: 10, right: 20, bottom: 60, left: 30},
     	height = barDiv.height() - margin.top - margin.bottom,
 		width = barDiv.width() - margin.left - margin.right;
+
+	var x = d3.scale.ordinal().rangeRoundBands([0,width], 0.05),
+		y = d3.scale.linear().range([height,0]);
+
+	var temp = findParty("2161 Ljusdal");
+	console.log(temp);
+
+	temp.sort(function(a,b){ return a.party > b.party; });
+	x.domain(temp.map(function(d){ return d.party+Object.keys(d)[2]; }));
+	y.domain([0, 60]);
+
+	var xAxis = d3.svg.axis()
+		.scale(x)
+		.orient("bottom")
+		.tickFormat(function(d,i){ console.log(d); return (i%3 == 1) ? parties[temp[i].party] : null; });
+
+	var yAxis = d3.svg.axis()
+		.scale(y)
+		.orient("left")
+		.ticks(10);
 
 	var colors = 
 	{
@@ -27,104 +50,68 @@ function barchart(){
 		"ogiltiga valsedlar":"#A9A9A9"
 	};
 
-	//https://bl.ocks.org/mbostock/3885304
-	var x = d3.scale.ordinal().rangeRoundBands([0,width], 0.05),
-		y = d3.scale.linear().range([height,0]);
-
-	var xAxis = d3.svg.axis()
-		.scale(x)
-		.orient("bottom")
-		.tickFormat(function(d,i){ console.log(d); return (i%3 == 1) ? temp[i].party : null; });
-
-	var yAxis = d3.svg.axis()
-		.scale(y)
-		.orient("left")
-		.ticks(10);
-
-	var svg = d3.select("#bar").append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom);
+	var parties = 
+	{
+		"Socialdemokraterna":"S", 
+		"Vänsterpartiet":"V", 
+		"Miljöpartiet":"MP", 
+		"Sverigedemokraterna":"SD", 
+		"Moderaterna":"M", 
+		"Kristdemokraterna":"KD",
+		"Centerpartiet":"C", 
+		"Folkpartiet":"FP", 
+		"övriga partier":"ÖVR", 
+		"ej röstande":"EJ", 
+		"ogiltiga valsedlar":"OG"
+	};
 	
+	var svg = d3.select("#bar").append("svg")
+		.attr("width", barDiv.width())
+		.attr("height", barDiv.height());
+
 	var g = svg.append("g")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	var dataTemp = [];
-	var dataSort = [];
+	g.append("g")
+		.attr("class", "x axis")
+		.attr("transform","translate(0," + height + ")")
+		.call(xAxis)
+		.selectAll("text")
+		.style("text-anchor","end")
+		.attr("dy","0.55em");
 
-	d3.csv("data/Swedish_Election_2002.csv", function(error, data){
-		dataTemp.push(data);
-		
-		d3.csv("data/Swedish_Election_2006.csv", function(error, data1){
-			dataTemp.push(data1);
-			
-			d3.csv("data/Swedish_Election_2010.csv", function(error, data2){
-				dataTemp.push(data2);
+	g.append("g")
+		.attr("class","y axis")
+		.call(yAxis)
+		.append("text")
+		.attr("transform","rotate(-90)")
+		.attr("y",6)
+		.attr("dy","0.71em")
+		.style("text-anchor","end");
 
-				dataTemp.forEach(function(d){
-					var t = d3.nest()
-						.key(function(v){ return v.region; })
-						.entries(d);
-					dataSort.push(t);
-				});
-
-				var t = [];
-
-				dataSort.forEach(function(d,i){
-					index = dataSort[i].findIndex(function(x,i){ if(x.key=="2161 Ljusdal"){ return i; } });
-					
-					dataSort[i][index].values.forEach(function(d){
-						t.push(d);
-					});
-				});
-
-				console.log(t);
-
-				x.domain(t.map(function(d){ return d.party+Object.keys(d)[2]; }));
-				y.domain([0, d3.max(t, function(d){ return (d[Object.keys(d)[2]] != "..") ? parseFloat(d[Object.keys(d)[2]]): 0; })]);
-
-				g.append("g")
-						.attr("class","x axis")
-						.attr("transform","translate(0," + height + ")")
-						.call(xAxis)
-						.selectAll("text")
-						.style("text-anchor","end")
-						.attr("dx","-0.8em")
-						.attr("dy","-0.55em")
-						.attr("transform","rotate(-75)");
-
-
-				console.log("hej");
-
-				g.append("g")
-					.attr("class","y axis")
-					.call(yAxis)
-					.append("text")
-					.attr("transform","rotate(-90)")
-					.attr("y",6)
-					.attr("dy","-0.71em")
-					.style("text-anchor","end");
-
-			});	
-		});
-	});
 
 	function draw(region){
-		
 		region.sort(function(a,b){ return a.party > b.party; });
 
 		x.domain(region.map(function(d){ return d.party+Object.keys(d)[2]; }));
-		y.domain([0, d3.max(region, function(d){ return (d[Object.keys(d)[2]] != "..") ? parseFloat(d[Object.keys(d)[2]]): 0; })]);
+		y.domain([0, d3.max(region, function(d){ return parseFloat(d[Object.keys(d)[2]]); })]);
 
-		g.selectAll(".bar")
+		g.selectAll("rect")
 			.data(region)
-			.enter().append("rect")
+			.enter()
+			.append("rect")
 			.style("fill",function(d){ return colors[d.party]; })
-			.attr("x", function(d){ 
-				return (d[Object.keys(d)[2]] != "..") ? x(d.party+Object.keys(d)[2]) : 0;
+			.attr("x", function(d,i){
+				return x(d.party+Object.keys(d)[2]);
+			})
+			.attr("y", function(d){
+				//console.log(height - y(d[Object.keys(d)[2]]));
+				return y(d[Object.keys(d)[2]]);
 			})
 			.attr("width", x.rangeBand())
-			.attr("y", function(d){ return y(d[Object.keys(d)[2]]); })
-			.attr("height", function(d){ return height - y(d[Object.keys(d)[2]]); })
+			.attr("height", function(d){
+				return height - y(d[Object.keys(d)[2]]);
+			})
 			.on("mousemove", function(d,i) {
 				var currentRegion = this;
 				toolTip.transition()
@@ -133,31 +120,39 @@ function barchart(){
                 toolTip.html(d.party + "<br>" + d[Object.keys(d)[2]])
                     .style("left", (d3.event.pageX) + "px")
                     .style("top", (d3.event.pageY) + "px");
+			})
+			.on("mouseout", function(d,i){
+				
+				toolTip.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+				
 			});
 	}
 
 	this.selectRegion = function(value){
-		var index = 0;
-		var temp = [];
+		svg.selectAll("rect").remove();
 
-		console.log(value);
-        
-		//index = self.data.findIndex(function(x,i){ if(x.key==value){ return i; } });
+		g.select(".x.axis").call(xAxis);
+		g.select(".y.axis").call(yAxis);
 
-		dataSort.forEach(function(d,i){
-			index = dataSort[i].findIndex(function(x,i){ if(x.key==value){ return i; } });
-			
-			dataSort[i][index].values.forEach(function(d){
-				temp.push(d);
-			});
-		});
-		
-		svg.selectAll(".bar").remove();
-
-		console.log(temp);
-		
-		draw(temp);
+		draw(findParty(value));
 	}
 
-	
+	function findParty(key){
+		var t = [];
+		var index = 0;
+
+		self.data.forEach(function(d,i){
+			index = self.data[i].findIndex(function(x,j){
+				if(x.key == key) return j;
+			});
+
+			d[index].values.forEach(function(x){
+				t.push(x);
+			});
+		});
+
+		return t;
+	}
 }
--- js/main.js
+++ js/main.js
@@ -2,8 +2,48 @@ var map;
 
 var piepop = new piePop();
 var pie1 = new pie();
-var bar = new barchart();
 
-d3.csv("data/Swedish_Election_2010.csv", function(error, data){
-	map = new map(data);
+/*d3.csv("data/Swedish_Election_2010.csv", function(error, data){
+	
+});*/
+
+var dataTemp = [];
+var dataSort = [];
+
+d3.csv("data/Swedish_Election_2002.csv", type, function(error, data){
+	dataTemp.push(data);
+	
+	d3.csv("data/Swedish_Election_2006.csv", type, function(error, data1){
+		dataTemp.push(data1);
+		
+		d3.csv("data/Swedish_Election_2010.csv", type, function(error, data2){
+			dataTemp.push(data2);
+			map = new map(data2);
+
+			dataTemp.forEach(function(d){
+				var t = d3.nest()
+					.key(function(v){ return v.region; })
+					.entries(d);
+				dataSort.push(t);
+			});
+
+			bar = new barchart(dataSort);
+		});	
+	});
 });
+
+function type(d) {
+
+	if(d[Object.keys(d)[2]] != "..")
+	{
+		d[Object.keys(d)[2]] = +d[Object.keys(d)[2]];
+		
+	}
+	else
+	{
+		d[Object.keys(d)[2]] = 0;
+		
+	}
+	
+	return d;
+}
